efb309bc4759271dc76a41d6d5846f017c502059
Myaddib <Hotabich80@gmail.com>
2022-01-11 16:04:42 +0200

    Добавил картинки, и движение капли

--- src/ru/geekbrains/catch_the_drop/GameWindow.java
+++ src/ru/geekbrains/catch_the_drop/GameWindow.java
@@ -1,35 +1,57 @@
 package ru.geekbrains.catch_the_drop;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.io.IOException;
 
-public class GameWindow extends JFrame {
+public class GameWindow extends JFrame{
 
     private static GameWindow game_window;
+    private static long last_frame_time;
 
-    public static void main(String[] args) {
-        game_window = new GameWindow(); //класс + сылка объект окна
-        game_window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); //настройка окна при закрытие
-        game_window.setLocation(200, 100); //где будет появлятся окно
-        game_window.setSize(906, 478); // размер окна
-        game_window.setResizable(false); // запретить изменение окна
+    private static Image background;
+    private static Image game_over;
+    private static Image drop;
+
+    private static float drop_left = 200;
+    private static float drop_top = -100;
+    private static float drop_v = 200;
+
+    public static void main(String[] args) throws IOException {
+
+        background = ImageIO.read(GameWindow.class.getResourceAsStream("background.png"));
+        game_over = ImageIO.read(GameWindow.class.getResourceAsStream("game_over.png"));
+        drop = ImageIO.read(GameWindow.class.getResourceAsStream("drop.png"));
+        game_window = new GameWindow();
+        game_window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        game_window.setLocation(200, 100);
+        game_window.setSize(906, 478);
+        game_window.setResizable(false);
+        last_frame_time = System.nanoTime();
         GameField game_field = new GameField();
         game_window.add(game_field);
-        game_window.setVisible(true); // сделать видимым окно
+        game_window.setVisible(true);
     }
 
-    private static void onRepaint (Graphics g) { // рисовать
-        g.fillOval(10,10,200,100);
-        g.drawLine(13,72,400,400);
-        g.drawLine(208,55,400,400);
+    private static void onRepaint(Graphics g){
+        long current_time = System.nanoTime();
+        float delta_time = (current_time - last_frame_time) * 0.000000001f;
+        last_frame_time = current_time;
+
+        drop_top = drop_top + drop_v * delta_time;
+        g.drawImage(background, 0, 0, null);
+        g.drawImage(drop, (int) drop_left, (int) drop_top, null);
+//        g.drawImage(game_over, 280, 120, null);
     }
 
-    private static class GameField extends JPanel{ //класс понели
+    private static class GameField extends JPanel{
 
         @Override
-        protected void paintComponent (Graphics g){
-            super.paintComponent(g); //сначало он
+        protected void paintComponent(Graphics g){
+            super.paintComponent(g);
             onRepaint(g);
+            repaint();
         }
     }
 }
